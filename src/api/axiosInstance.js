259b6969ea7422cc659b8a75a80d2fc07c5ca2b1
general706 <general706@naver.com>
2025-07-13 17:58:20 +0900

    Update axiosInstance.js
    백엔드 써버 주소 체인지

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://3.36.64.208", // 여기에 실제 API 서버 URL 입력
+  baseURL: "https://blockchef.store/", // 여기에 실제 API 서버 URL 입력
   headers: {
     "Content-Type": "application/json",
     Accept: "*/*",
