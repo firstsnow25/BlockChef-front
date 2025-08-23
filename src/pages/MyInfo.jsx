e55bf03e730f8a557c744a50efc584e8e17f5382
general706 <general706@naver.com>
2025-08-23 09:05:20 +0900

    Update MyInfo.jsx

--- src/pages/MyInfo.jsx
+++ src/pages/MyInfo.jsx
@@ -70,7 +70,7 @@ export default function MyInfo() {
             <span>●●●●●●</span>
           </div>
         </div>
-        <div className="mt-6 flex gap-4">
+        <div className="mt-6 flex items-center justify-between max-w-md">
           <button
             onClick={handleDelete}
             className="bg-red-300 text-white px-5 py-2 rounded-full"
@@ -88,6 +88,7 @@ export default function MyInfo() {
     </div>
   );
 }
+
 
 
 
